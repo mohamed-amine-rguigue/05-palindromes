ce75b2fccc83e58c4d231a6a5e8378be41f14c7d
mohamed-amine.rguigue <mohamed-amine.rguigue@edu.esiee.fr>
2025-09-22 13:40:12 +0000

    Exercice fini!

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+"""Fonctions de palindrome"""
 #### Fonction secondaire
 
-
 def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
+    """A-t-on des palindromes?"""
+    a=0
+    for k in p:
+        if k != p[(len(p)-1-a)]:
+            return False
+        a+=1
+    return True
 
 #### Fonction principale
-
-
 def main():
-
+    """Test de différents palindromes"""
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
